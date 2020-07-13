c250637b88e6cf84a1f537c779990df0b5046e8a
Romario Gustave <romagustave10@gmail.com>
2020-07-13 01:27:09 +0000

    Automatically backed up by Learn

--- db/migrate/20200713012522_create_pirates.rb
+++ db/migrate/20200713012522_create_pirates.rb
@@ -2,5 +2,7 @@ class CreatePirates < ActiveRecord::Migration[6.0]
   def change
     create_table :pirates do |t|
       t.string :name
+        t.string :height
+          t.string :weight
   end
 end
